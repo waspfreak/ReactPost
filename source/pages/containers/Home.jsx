b2a3e461a6054dc13d9281adab924d09399b6ae1
zo17 <juliana.leon@global.tesco.org>
2016-12-31 20:29:11 +0000

    first commit

---
+++ source/pages/containers/Home.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+import Post from '../../posts/containers/Post.jsx';
+import Loading from '../../shared/components/Loading.jsx'
+
+import api from '../../api.js';
+
+class Home extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      page: 1,
+      posts: [],
+      loading: true,
+    };
+  }
+
+  async componentDidMount() {
+    const posts = await api.posts.getList(this.state.page);
+
+    this.setState({
+      posts,
+      page: this.state.page + 1,
+      loading: false,
+    })
+  }
+  render() {
+    return (
+      <section name="Home">
+        <h1>Home</h1>
+        <section>
+          {this.state.loading && (
+            <Loading />
+          )}
+          {this.state.posts
+            .map(post => <Post key={post.id} {...post} />)}
+        </section>
+      </section>
+    );
+  }
+}
+
+export default Home;
